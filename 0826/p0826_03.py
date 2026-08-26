a6f2f28f05097d955d87748dabcf0c05daf98661
lsh061965@gmail.com <lsh061965@gmail.com>
2026-08-26 11:59:43 +0900

    0826교육내용

---
+++ 0826/p0826_03.py
@@ -0,0 +1,44 @@
+# rum1 = 100
+# rum2 = 200
+# rum3 = 300
+# print(rum1, rum2, rum3)
+
+# num4=num5=num6=1  # 한줄에 여러값을 넣는것은 가능
+# print(num4, num5, num6)
+
+# a1=1, a2="안녕"  # 서로다른 변수는 한줄에 넣을 수 없다.
+# a1=1
+# a2="안녕"
+# print(a1, a2)
+
+# a = 10
+# b = 3
+# print(a+b)
+# print(a-b)
+# print(a*b)
+# print(a/b)
+# print(a**b) # a의 b승을 말함
+
+# print : 출력
+# input : 입력
+
+# num = input("숫자를 입력하세요")
+# print("입력숫자 : {}".format(num))
+
+# input로 입력받은 모든것은 문자열타입으로 인식한다.
+# a = int(input("첫번째 숫자를 입력하세요."))   # str타입을 int타입으로 변경
+# b = int(input("두번째 숫자를 입력하세요."))
+
+# print(a+b)
+# print(a-b)
+# print(a*b)
+# print(a/b)
+# print(a**b)
+
+# 아이디, 패스원드를 입력받아 출력하시요.
+# 아이지 : aaaa , 패스워드 : 1111
+
+name = input("아이디 :")
+pw = input("패스워드 :")
+print("아이디 : {}".format(name))
+print("패스워드 : {}".format(pw))
